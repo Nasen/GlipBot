dcf2bd3958582251254c9a81999a51bfe6196aec
nasen.you <nasen.you@ringcentral.com>
2017-04-08 09:52:43 +0800

    optimize message parse

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -17,77 +17,98 @@ module.exports = (robot)=> {
       flag = false;
       feedback = {};
       let text = message.text;
-      let description="", status="", contact_name="",
-        mailboxID="", device="", version="", os_version="", occur_time="";
+      let description = "", status = "", contact_name = "",
+        mailboxID = "", device = "", version = "", os_version = "", occur_time = "";
       //var re_subject = new RegExp("^Subject: RingCentral (\d+(\.\d+)*) (Android|iOS) Feedback (\+\d+ \(\d+\) \d+)\s*,\s*(.+) (.+)$");
 
-      if (typeof(text) == "string" && text.indexOf("RC Mobile Feedback") > -1 && message.room=='46352326658') {
+      if (typeof(text) == "string" && text.indexOf("RC Mobile Feedback") > -1 && message.room == '46352326658') {
         //console.log("message text:\n" + text);
         //console.log("text type:" + typeof (text));
         match = true;
 
         console.log("meessage info:\n" + JSON.stringify(message));
-        console.log("room: "+message.room);
+        console.log("room: " + message.room);
 
-        var items=text.split("\n");
+        var items = text.split("\n");
 
 
-        for(var i=items.length;i>=0;i--){
-          console.log("item_"+i+": "+items[i]);
-          if(items[i]==""){
-            items.splice(i,1);
+        for (var i = items.length; i >= 0; i--) {
+          console.log("item_" + i + ": " + items[i]);
+          if (items[i] == "") {
+            items.splice(i, 1);
           }
         }
 
-        for(var i=0;i<items.length;i++){
-          console.log("new_"+i+":"+items[i]);
+        for (var i = 0; i < items.length; i++) {
+          console.log("new_" + i + ":" + items[i]);
         }
 
-        var from=items[0];
-        console.log("from= " +from);
-        contact_name=from.split("<")[0].split('From:')[1]+"<"+from.split('<')[1]+">";
-        console.log("contact name:"+ contact_name);
-        var data=items[1];
-        occur_time=data.split("Date: ")[1];
-        console.log("occur time:"+occur_time);
-        var subject=items[3];
-        console.log("subject:"+subject);
+        var from = items[0];
+        console.log("from= " + from);
+        contact_name = from.split("<")[0].split('From:')[1] + "<" + from.split('<')[1] + ">";
+        console.log("contact name:" + contact_name);
+        var data = items[1];
+        occur_time = data.split("Date: ")[1];
+        console.log("occur time:" + occur_time);
+        var subject = items[3];
+        console.log("subject:" + subject);
 
 
-        var info=subject.match(new RegExp("^Subject: RingCentral (\\d+(\\.\\d+)*) (Android?) Feedback (\\+\\d+ \\(\\d+\\) \\d+)\\s*,\\s*(.+) (.+)$"));
+        var info
+        if(new RegExp("Android Feedback").test(subject)){
+          console.log(">>>>?1");
+          info = subject.match(new RegExp("^Subject: RingCentral (\\d+(\\.\\d+)*) (Android?) Feedback (\\+\\d+ \\(\\d+\\) \\d+)\\s*,\\s*(.+)/(.+)$"));
+          for (var i = 0; i < info.length; i++) {
+            console.log(i + ": " + info[i]);
+          }
 
-        for(var i=0;i<info.length;i++){
-          console.log(i+": "+info[i]);
+          version = info[1];
+          device = info[5];
+          os_version = info[6];
+          console.log("Version: " + version);
+        }else{
+          console.log(">>>>?2");
+          info=subject.match(new RegExp("^Subject: RingCentral (\\d+(\\.\\d+)*) Feedback (\\+\\d+ \\(\\d+\\) \\d+ \\* \\d+)\\s*,\\s*(.+)/(.+)$"));
+          for (var i = 0; i < info.length; i++) {
+            console.log(i + ": " + info[i]);
+          }
+          version = info[1];
+          device = info[5];
+          os_version = info[6];
+          console.log("Version: " + version);
         }
 
-        version=info[1];
-        device=info[5];
-        os_version=info[6];
-        console.log("Version: "+version);
 
-        for(var i=items.length;i>3;i--){
-          var re=new RegExp("^RingCentral");
-          if(re.test(items[i])){
-            for(var j=4;j<i;j++){
-              var er=new RegExp("^Please describe your problem here");
-              if(er.test(items[i+1])){
-                description+=items[i+1]+"/n";
-              }else{
-                description+=items[i]+"/n";
+        for (var i = items.length; i > 3; i--) {
+          var re = new RegExp("^RingCentral");
+          if (re.test(items[i])) {
+            var er = new RegExp("^Please describe your problem here");
+
+            if (er.test(items[4])) {
+              for (var j = 5; j < i; j++) {
+                console.log(">>>>>>1" + items[j]);
+                description += items[j] + "/n";
+              }
+            } else {
+              for (var j = 4; j < i; j++) {
+                console.log(">>>>>>2" + items[j]);
+                description += items[j] + "/n";
               }
             }
 
-            console.log("this msg: "+items[i]);
-            var temp=items[i].split(', ');
-            console.log("temp length:"+temp.length);
-            mailboxID=temp[temp.length-1];
+
+            console.log("this msg: " + items[i]);
+            var temp = items[i].split(', ');
+            console.log("temp length:" + temp.length);
+            mailboxID = temp[temp.length - 1];
             break;
+          }else{
+            //TODO
           }
         }
 
-        console.log("description: "+description);
-        console.log("mailboxID: "+mailboxID);
-
+        console.log("description: " + description);
+        console.log("mailboxID: " + mailboxID);
 
 
         feedback = {
